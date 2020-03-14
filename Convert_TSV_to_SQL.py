c077e982c06b8e7715aaab402c0b38cce20cea14
Rory Coffey <coffeyrt@gmail.com>
2020-03-14 17:14:27 -0400

    Updated to current syntax after leaving it put for a long time.  Still needs some cleaning

--- Convert_TSV_to_SQL.py
+++ Convert_TSV_to_SQL.py
@@ -4,74 +4,102 @@
 Created on Sat Feb 16 13:57:27 2019
 
 @author: Rory
+
+This file converts TSV (tab separated files) to input files for MySQL
+Afterwards:
+    mysql -u <username> -p < <sql_file>
 """
 
 import argparse
 import csv
-'''
-This file converts TSV (tab separated files) to input files for MySQL
-Afterwards:
-    mysql -u <username> -p < <sql_file>
-'''
+import sys
+
+from typing import List, Tuple
 
 
 def arguments():
     '''
     Gets the arguments from command line
     '''
-    parser = argparse.ArgumentParser(description = "Converts TSV file to MySQL importable file", usage = "python %(prog)s [-options] <TSV_file>")
-    parser.add_argument("TSV_file", type = str, help = "file.tsv file to be converted (required)")
-    parser.add_argument("-o", dest = 'sql_file', type = str, help = "SQL file name for output (default file.sql)", required = False)
-    parser.add_argument("-t", dest = "Table_name", type = str, help = "Table name for SQL import (default file)", required = False)
-    parser.add_argument("-p", dest = "primary_first", action = "store_true", default = False, help = "First column used as a primary key (default False)", required = False)
+    parser = argparse.ArgumentParser(
+        description="Converts TSV file to MySQL importable file",
+        usage="python %(prog)s [-options] <TSV_file>")
+    parser.add_argument("TSV_file", type=str, help="file.tsv file to be converted (required)")
+    parser.add_argument("-o", dest='sql_file', type=str,
+                        help="SQL file name for output (default file.sql)", required=False)
+    parser.add_argument("-t", dest="Table_name", type=str,
+                        help="Table name for SQL import (default file)", required=False)
+    parser.add_argument("-p", dest="primary_first", action="store_true", default=False,
+                        help="First column used as a primary key (default False)", required=False)
     args = parser.parse_args()
     return args
-    
 
-def transform_tab(data):
+
+def transform_tab(data: str) -> str:
     '''
     Removes the tabs and replaces them with ","s which is needed for MySQL
     Also adds '`' to the column names
     For data values, adds "'"s to string columns and leaves number data without quotes
+    :data: 
+    :return: reformed data
     '''
-    data = data.replace("\n","")
-    data = data.replace("\t","`,`")
-    data = "`" + data + "`"
+    data = data.replace("\n", "")
+    data = data.replace("\t", "`,`")
+    data = f"`{data}`"
     return data
 
-def is_character(data_types):
+
+def is_character(data_types: List[str]) -> List[int]:
+    """
+    :data_types:
+    :return:
+    """
     is_char = []
     for x in range(len(data_types)):
-        if data_types[x].find("CHAR") > 0:
+        if data_types[x].find("CHAR"):
             is_char.append(x)
     return is_char
 
-def add_quotes(row, is_char):
+
+def add_quotes(row: List[str], is_char: List[int]) -> List[str]:
+    """
+    Adds quotes around string values in rows
+    :row: a single row from the tsv file
+    :is_char: a list of indexes that are texts instead of numbers
+    :return: row with quotes around string values
+    """
     for x in is_char:
-        row[x] = "'" + row[x] + "'"
+        row[x] = f"'{row[x]}'"
     return row
 
-def get_data(tsv_file, data_types):
+
+def get_data(tsv_file: str, data_types: List[str]) -> Tuple[str, int]:
     '''
     Creates the values input for MySQL which is () encapsulated per row with a "," afterwards
+    :tsv_file:
+    :data_types:
+    :return:
     '''
     is_char = is_character(data_types)
     with open(tsv_file, "r") as tsv:
-        tsvreader = csv.reader(tsv, delimiter = "\t")
+        tsvreader = csv.reader(tsv, delimiter="\t")
         tsv_data = list(tsvreader)
     tsv_data = tsv_data[1:]
-    for x in range(len(tsv_data)):
-        if len(is_char) > 0:
-            tsv_data[x] = add_quotes(tsv_data[x], is_char)
-        tsv_data[x] = ",".join(tsv_data[x])
+    for x, row in enumerate(tsv_data):
+        if len(is_char):
+            row = add_quotes(row, is_char)
+        tsv_data[x] = ",".join(row)
     data = "),\n(".join(tsv_data)
-    data = "(" + data + ")"
+    data = f"({data})"
     return data, len(tsv_data)
 
-def get_columns(tsv_file):
+
+def get_columns(tsv_file: str) -> Tuple[str, List[str]]:
     '''
     Gets the column names for MySQL input
     Also finds whether each column is a string or a number
+    :tsv_file:
+    :return:
     '''
     with open(tsv_file, "r") as tsv:
         columns = tsv.readline()
@@ -85,38 +113,46 @@ def get_columns(tsv_file):
             data_types.append("VARCHAR(20)")
     columns = transform_tab(columns)
     return columns, data_types
-    
-def write_file(filename, columns, data_types, data, linecount, table_name, primary_first):
+
+
+def write_file(filename: str, columns: str, data_types: List[str], data: str, linecount: int,
+               table_name: str, primary_first):
     '''
     Pulls everything together to create an SQL input file
+    :filename:
+    :columns:
+    :data_types:
+    :data: refurmed data into sql format for import
+    :linecount: number of lines in TSV file/database
+    :table_name:
+    :primary_first:
+    :return:
     '''
     with open(filename, "w+") as file:
-        file.write("DROP TABLE IF EXISTS "+ table_name +";\n")
-        file.write("CREATE TABLE "+ table_name +"(\n")
-        for x in range(len(columns.split(","))):
+        file.write(f"DROP TABLE IF EXISTS {table_name};\n")
+        file.write(f"CREATE TABLE {table_name}(\n")
+        for x, column in enumerate(columns.split(",")):
             if x == 0:
                 if primary_first:
-                    file.write(columns.split(",")[x] + " "+ data_types[x] +" NOT NULL,\n")
+                    file.write(f"{column} {data_types[x]} NOT NULL,\n")
                 else:
                     file.write("id INT NOT NULL AUTO_INCREMENT,\n")
-                    file.write(columns.split(",")[x] + " "+ data_types[x] + ",\n")
+                    file.write(f"{column} {data_types[x]},\n")
             else:
-                file.write(columns.split(",")[x] + " "+ data_types[x] + ",\n")
+                file.write(f"{column} {data_types[x]},\n")
         if primary_first:
-            file.write("PRIMARY KEY ( "+ columns.split(",")[0] +" )")
+            file.write(f"PRIMARY KEY ( {columns.split(',')[0]} )")
         else:
             file.write("PRIMARY KEY ( id )")
         file.write(");")
-        file.write("\n\nINSERT INTO "+ table_name +"\n(")
+        file.write(f"\n\nINSERT INTO {table_name}\n(")
         file.write(columns)
         file.write(")\nVALUES\n")
-        file.write(data)    
+        file.write(data)
         file.write(";")
-        
-def run():
-    '''
-    Runs all the functions above together
-    '''
+
+
+def main():
     args = arguments()
     columns, data_types = get_columns(args.TSV_file)
     print("Column names imported")
@@ -124,13 +160,16 @@ def run():
     if args.sql_file is not None:
         filename = args.sql_file
     else:
-        filename = args.tsv_file[:args.TSV_file.upper().find(".TSV")] + ".sql"
+        filename = args.TSV_file[:args.TSV_file.upper().find(".TSV")] + ".sql"
     if args.Table_name is not None:
         table_name = args.Table_name
     else:
-        table_name = args.tsv_file[:args.TSV_file.upper().find(".TSV")]
+        table_name = args.TSV_file[:args.TSV_file.upper().find(".TSV")]
     print("Writing")
     write_file(filename, columns, data_types, data, linecount, table_name, args.primary_first)
-    
+
+
 if __name__ == "__main__":
-    run()
+    if float(f"{sys.version_info[0]}.{sys.version_info[1]}") < 3.6:
+        raise SystemError("Python 3.6 or newer required")
+    main()
