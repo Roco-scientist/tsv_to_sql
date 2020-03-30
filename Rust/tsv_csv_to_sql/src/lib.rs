15e9b22fb79f253c87fb6393926e3c37ba576b11
Rory Coffey <coffeyrt@gmail.com>
2020-03-29 21:22:44 -0400

    added csv support.  Still working on converting all of the code

--- Rust/tsv_csv_to_sql/src/lib.rs
+++ Rust/tsv_csv_to_sql/src/lib.rs
@@ -1,16 +1,35 @@
+enum Filetype {
+    CSV,
+    TSV,
+}
+
 pub fn transform_tab(data: String) -> String {
     // Removes the tabs and replaces with ","
     let mut reformed_data = data.replace("\t", "`,`");
     reformed_data.push_str("`");
     let data_preceder = String::from("`");
-    return data_preceder + &reformed_data
+    return data_preceder + &reformed_data;
+}
+
+pub fn transform_csv(data: String) -> String {
+    // Removes the commas and replaces with ","
+    let mut reformed_data = data.replace(",", "`,`");
+    reformed_data.push_str("`");
+    let data_preceder = String::from("`");
+    return data_preceder + &reformed_data;
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_transform_tab(){
-        assert_eq!(transform_tab(String::from("one\ttwo\tthree")), String::from("`one`,`two`,`three`"));
+    fn test_transform_tab() {
+        let header_ouput = String::from("`one`,`two`,`three`");
+        assert_eq!(transform_tab(String::from("one\ttwo\tthree")), header_ouput);
+    }
+    #[test]
+    fn test_transform_csv() {
+        let header_ouput = String::from("`one`,`two`,`three`");
+        assert_eq!(transform_csv(String::from("one,two,three")), header_ouput);
     }
 }
